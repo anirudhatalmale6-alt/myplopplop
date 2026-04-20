ed9fb05076093f846c4f7e9f5616da05569c3f82
Anirudha Talmale <anirudhatalmale6@gmail.com>
2026-04-20 17:34:05 +0000

    Change Send Support button to 'Sipòte Fanmiw'

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // MyPlopPlop Service Worker v11
 // Cache-first for static assets, network-first for HTML pages
 
-const CACHE_NAME = 'myplopplop-v51';
+const CACHE_NAME = 'myplopplop-v52';
 
 const PRE_CACHE_URLS = [
   '/',
